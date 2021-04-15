529ff2de92c5236281453acf55f5507a123f7e3d
eeelian8 <elianleguizamon@gmail.com>
2021-04-14 22:35:28 -0300

    Se agregaron de manera dinamica los divs

--- js/loops.js
+++ js/loops.js
@@ -1,46 +1,45 @@
-// array, arreglo, matriz, lista, vector
+// // array, arreglo, matriz, lista, vector
 
-// var nombre = [
-//   'Alejandro', 
-//   'Ezequiel',
-//   'Villafane',
-// ]
+// // var nombre = [
+// //   'Alejandro', 
+// //   'Ezequiel',
+// //   'Villafane',
+// // ]
 
-// var mixto = [
-//   'Soy un string',
-//   11,
-//   null,
-//   true,
-// ]
+// // var mixto = [
+// //   'Soy un string',
+// //   11,
+// //   null,
+// //   true,
+// // ]
 
-// console.log(mixto[0])
-// // console.log(mixto.length) para ver el total
+// // console.log(mixto[0])
+// // // console.log(mixto.length) para ver el total
 
-// mixto.push('nuevo dato')
+// // mixto.push('nuevo dato')
 
 
-// // Iteraciones
-// // Desde - Hasta
-// // salto
+// // // Iteraciones
+// // // Desde - Hasta
+// // // salto
 
-// for (var i = 0; i < 11; i++) {
-//   console.log(i)
+// // for (var i = 0; i < 11; i++) {
+// //   console.log(i)
+// // }
+
+
+
+// //  Array Asociativo
+// var usuario = {
+//   primerNombre : 'Elian',
+//   apellido: 'Leguizamon',
+  
 // }
 
-
-
-//  Array Asociativo
-var usuario = {
-  primerNombre : 'Elian',
-  apellido: 'Leguizamon',
-  
-}
-
-//  usuario.(ejemplo)  usuario.primerNombre
+// //  usuario.(ejemplo)  usuario.primerNombre
 
 
 var linksContainer = document.querySelector('.second-nav-links')
-
 var links = [
   {
     href: '#home',
@@ -59,14 +58,36 @@ var links = [
     name: 'Contacto',
   }
 ]
-
-// var links = [
-//   'home',
-//   'Nosotros',
-//   'Productos',
-//   'Contacto',
-// ]
-
+var links = [
+  'home',
+  'Nosotros',
+  'Productos',
+  'Contacto',
+]
 for (var i = 0; i < links.length; i++) {
   linksContainer.innerHTML += '<a href=""' + links[i].href + '>' + links[i].name + '</a>'
+}
+
+// ------------------------------------------
+
+var divContainer = document.querySelector('#items')
+var divs = [
+  {
+    img:'https://placeimg.com/400/400/arch',
+    p:'Product category one',
+  }
+]
+for (var i = 0; i < divs.length; i++) {
+  divContainer.innerHTML += '<div class="item-product">' + '<img src="' + divs[i].img + '"' + '>' + '<p><span>' + divs[i].p + '</span></p>' + '</div>'
+  divContainer.innerHTML += '<div class="item-product">' + '<img src="' + divs[i].img + '"' + '>' + '<p><span>' + divs[i].p + '</span></p>' + '</div>'
+  divContainer.innerHTML += '<div class="item-product">' + '<img src="' + divs[i].img + '"' + '>' + '<p><span>' + divs[i].p + '</span></p>' + '</div>'
+  divContainer.innerHTML += '<div class="item-product">' + '<img src="' + divs[i].img + '"' + '>' + '<p><span>' + divs[i].p + '</span></p>' + '</div>'
+  divContainer.innerHTML += '<div class="item-product">' + '<img src="' + divs[i].img + '"' + '>' + '<p><span>' + divs[i].p + '</span></p>' + '</div>'
+  divContainer.innerHTML += '<div class="item-product">' + '<img src="' + divs[i].img + '"' + '>' + '<p><span>' + divs[i].p + '</span></p>' + '</div>'
+  divContainer.innerHTML += '<div class="item-product">' + '<img src="' + divs[i].img + '"' + '>' + '<p><span>' + divs[i].p + '</span></p>' + '</div>'
+  divContainer.innerHTML += '<div class="item-product">' + '<img src="' + divs[i].img + '"' + '>' + '<p><span>' + divs[i].p + '</span></p>' + '</div>'
+  divContainer.innerHTML += '<div class="item-product">' + '<img src="' + divs[i].img + '"' + '>' + '<p><span>' + divs[i].p + '</span></p>' + '</div>'
+  divContainer.innerHTML += '<div class="item-product">' + '<img src="' + divs[i].img + '"' + '>' + '<p><span>' + divs[i].p + '</span></p>' + '</div>'
+  divContainer.innerHTML += '<div class="item-product">' + '<img src="' + divs[i].img + '"' + '>' + '<p><span>' + divs[i].p + '</span></p>' + '</div>'
+  divContainer.innerHTML += '<div class="item-product">' + '<img src="' + divs[i].img + '"' + '>' + '<p><span>' + divs[i].p + '</span></p>' + '</div>'
 }
